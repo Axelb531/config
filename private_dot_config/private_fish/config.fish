cf6a0b94bbc24423e92881d4742e08d9a805a7bd
Axel Sneitz-Björkman <axel.sneitz@nordaxon.com>
2026-05-20 22:50:37 +0200

    Add current fish config: config.fish, fish_variables, envsource, fish_prompt

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,17 +1,38 @@
+# Adjust error for GPG agent
 set -gx GPG_TTY (tty)
 
+export GPG_TTY=(tty)
 
-# Setting PATH for Python 3.12
-# The original version is saved in /Users/axel/.config/fish/config.fish.pysave
-set -x PATH "/Library/Frameworks/Python.framework/Versions/3.12/bin" "$PATH"
+# Personal aliases
+function cd_up
+    set levels $argv[1]
 
-function envsource
-  for line in (cat $argv | grep -v '^#')
-    set item (string split -m 1 '=' $line)
-    set -gx $item[1] $item[2]
-    echo "Exported key $item[1]"
-  end
+    if test -z "$levels"
+        echo "Usage: cd_up <n>"
+        return 1
+    end
+
+    set path (string repeat -n $levels "../")
+    cd $path
 end
 
-# opencode
-fish_add_path /Users/axel/.opencode/bin
+abbr -a cd.. cd_up
+
+alias zed="open -a /Applications/Zed.app -n"
+
+alias civenso="cd ~/Developer/civenso-chatbot-api"
+
+alias python=python3
+
+alias pip=pip3
+
+alias note='fish -c ~/Notes/createDailyNote.fish'
+
+alias docekr=docker
+
+alias ll='ls -l'
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/axel/Downloads/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/axel/Downloads/google-cloud-sdk/path.fish.inc'
+end
